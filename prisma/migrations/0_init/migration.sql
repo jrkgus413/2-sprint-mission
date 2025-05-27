d500b0731d4ec08621fb4646d58d32fca0c90276
jrkgus413 <jrkgus413@naver.com>
2025-05-27 18:07:00 +0900

    패키지 추가 및 코드 정리리

--- prisma/migrations/0_init/migration.sql
+++ prisma/migrations/0_init/migration.sql
@@ -4,6 +4,7 @@ CREATE TABLE "Product" (
     "name" TEXT NOT NULL,
     "description" TEXT,
     "price" DOUBLE PRECISION NOT NULL,
+    "tags" TEXT[],
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
